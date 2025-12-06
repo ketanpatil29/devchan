e43424e4e31f9a5c7c0d5536680e0383c89be427
Ketan patil <ketanpatil.sh@gmail.com>
2025-12-06 18:03:36 +0530

    Update working on dashboard

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -6,6 +6,8 @@ const Dashboard = () => {
   const [username, setUsername] = useState("");
   const [token, setToken] = useState("");
 
+  const [ openProfileMenu, setOpenProfileMenu ] = useState(false);
+
   useEffect(() => {
     const t = searchParams.get("token");
     const u = searchParams.get("username");
@@ -22,14 +24,36 @@ const Dashboard = () => {
   }, [searchParams]);
 
   return (
-    <div className="flex flex-col items-center justify-center h-screen">
-      <h1 className="text-5xl mb-4">DEVCHAN Dashboard</h1>
-      {username || token ? (
-        <p className="text-green-700 text-xl">Welcome, {username}!</p>
-      ) : (
-        <p className="text-red-500">You are not logged in.</p>
-      )}
-    </div>
+    <section className="relative h-screen">
+
+      <header className="fixed top-0 left-0 w-full h-16 flex items-center px-6 z-50">
+        <div className="ml-auto">
+          <button className="bg-emerald-400 px-3 py-2 rounded text-xl">Logout</button>
+          <button className="bg-emerald-400 px-4 py-2 rounded-[50%] text-xl ml-2 cursor-pointer" onClick={() => {setOpenProfileMenu(!openProfileMenu)}}>
+            P
+          </button>
+
+          {openProfileMenu && 
+            (
+            <div className="absolute top-16 right-6 bg-white border shadow-lg rounded-md w-48 py-2 z-50">
+              <button className="block w-full text-left px-4 py-2 hover:bg-gray-100">Change Profile</button>
+              <button className="block w-full text-left px-4 py-2 hover:bg-gray-100">Settings</button>
+              <button className="block w-full text-left px-4 py-2 hover:bg-gray-100">Set Status</button>
+              <button className="block w-full text-left px-4 py-2 hover:bg-gray-100 text-red-500">Logout</button>
+            </div>
+            )
+          }
+        </div>
+      </header>
+      <div className="pt-24 flex flex-col items-center">
+        <h1 className="text-5xl mb-4">DEVCHAN Dashboard</h1>
+        {username ? (
+          <p className="text-green-700 text-xl">Welcome, {username}!</p>
+          ) : (
+          <p className="text-red-500">You are not logged in.</p>
+        )}
+      </div>
+    </section>
   );
 };
 
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Login = () => {
   return (
-    <div className="flex flex-col items-center justify-center h-screen">
+    <div className="bg-linear-to-t from-[#dce6d8] to-[#dce6d8] flex flex-col items-center justify-center h-screen">
       <h1 className="exo-font font-bold absolute top-40 left-135 text-[100px] mb-4">DEV</h1>
       <h1 className="exo-font font-light absolute top-40 left-183 text-[100px] mb-4">ELOPER</h1>
       <h1 className="exo-font font-bold absolute top-70 left-250 text-[100px] mb-4">CHAN</h1>
