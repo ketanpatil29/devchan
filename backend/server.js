ddb83b6fdc13d1af746cf02c346fe14094484da2
Ketan patil <ketanpatil.sh@gmail.com>
2025-12-10 19:44:35 +0530

    update backend file

--- backend/server.js
+++ backend/server.js
@@ -43,6 +43,8 @@ server.get("/", (req, res) => {
     res.send("Server listening...")
 })
 
-server.listen(process.env.PORT || 3000, () => {
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
     console.log(`Server is running on ${PORT}`);
-})
+});
