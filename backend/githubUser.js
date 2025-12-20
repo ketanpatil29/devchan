f3c3814249f70df861042456a7fe93f842fc7534
Ketan patil <ketanpatil.sh@gmail.com>
2025-12-20 19:17:49 +0530

    update populate in githubUser.js

--- backend/githubUser.js
+++ backend/githubUser.js
@@ -8,7 +8,7 @@ router.get("/me/:username", async (req, res) => {
   try {
     const username = req.params.username;
     const user = await User.findOne({ username })
-      .populate("friendRequests.from", "username avatar");
+      .populate("friends", "username avatar");
 
     if (!user) return res.status(404).json({ message: "User not found" });
 
